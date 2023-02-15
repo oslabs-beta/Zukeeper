d006ac5052b79a86d19fe08dd0ff6b2f66ecadc5
ctolmsted <prunes03smugger@icloud.com>
2023-02-15 18:33:46 -0500

    progress on diff bug fix

--- src/client/components/DiffDisplay.tsx
+++ src/client/components/DiffDisplay.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import useExtensionStore from '../store/useExtensionStore';
 import { useStore } from 'zustand';
 const diff = require('deep-diff').diff;
-import { DiffItem } from './DiffItem'
-import { diffProps } from '../../types'
+import { DiffItem } from './DiffItem';
+import { diffProps } from '../../types';
 
 export const DiffDisplay = () => {
-  
-  const { previousStates }  = useStore(useExtensionStore);
-  const differences = diff(previousStates[previousStates.length - 2], previousStates[previousStates.length - 1]);
+  const { previousStates } = useStore(useExtensionStore);
+  if (
+    previousStates[previousStates.length - 2]
+  ) {
+    const differences = diff(
+      previousStates[previousStates.length - 2],
+      previousStates[previousStates.length - 1]
+    );
 
-  const diffItems = differences.map( (obj: diffProps) => {
-    return <DiffItem obj={obj}></DiffItem>
-  })
+    const diffItems = differences.map((obj: diffProps) => {
+      return <DiffItem obj={obj}></DiffItem>;
+    });
 
-  return (
-    <div>{diffItems}</div>
-  );
+    return <div>{diffItems}</div>;
+  } else return null;
 };
