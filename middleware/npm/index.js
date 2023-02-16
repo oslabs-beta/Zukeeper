ead37db8700226c391a523ae902bbad12e148cd6
ctolmsted <prunes03smugger@icloud.com>
2023-02-16 12:25:11 -0500

    updated npm files for diff debug and ts/js debug

--- middleware/npm/index.js
+++ middleware/npm/index.js
@@ -1,3 +1,5 @@
+import { create } from "zustand";
+
 function zukeeper(createFunction) {
   return (set, get) => {
     const store = createFunction(set, get);
@@ -5,16 +7,21 @@ function zukeeper(createFunction) {
       if (typeof store[key] === 'function') {
         let functionDefinition = store[key];
         store[key] = (...args) => {
+          let currstate = get();
+          window.postMessage({
+            body: 'Innit',
+            state: JSON.stringify(currstate)
+          });
           functionDefinition(...args);
-          const currstate = get();
+          currstate = get();
           window.postMessage({
             body: 'Data',
             state: JSON.stringify(currstate),
             actions: key,
           });
         };
-      }
-    }
+      };
+    };
     return store;
   };
 }
