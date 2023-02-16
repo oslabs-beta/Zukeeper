7b2fcbcd886dade8d97ef5435354b444b180076c
Santi Lorenzo <santirlorenzo@gmail.com>
2023-02-16 11:45:39 -0500

    formatted/updated devtool

--- src/client/components/ActionsDispatched.tsx
+++ src/client/components/ActionsDispatched.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import useExtensionStore from '../store/useExtensionStore';
 import { useStore } from 'zustand';
+import { Action } from './Action'
+import './ActionsDispatched.scss'
 
 export const ActionsDispatched = () => {
   const { actionsDispatched }  = useStore(useExtensionStore);
  
   const actions = actionsDispatched.map((el, idx) => {
-    return <li key={idx}>{el}</li>
+    // return <li key={idx}>{el}</li>
+    return <Action key={idx} action={el}/>
   });
 
   return(
-    <div id="actions-dispatched-container">
-      <h2 id="action-header">Actions Dispatched</h2>
-      <div id="action-list">
-        <ul>
-          {actions}
-        </ul>
+    <div className="actions-container">
+      <input className="actions-input" type="text" placeholder='filter actions...' autoFocus/>
+      <div className="actions-list">
+        {actions}
       </div>
     </div>
   );
