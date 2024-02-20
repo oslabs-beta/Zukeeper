0d8f4162815158b4b1a9cd340a617764cb546918
ctolmsted <prunes03smugger@icloud.com>
2024-02-20 12:12:07 -0500

    commented out webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,70 @@
+/*
+This is the webpack configuration file for the Chrome extension's background script
+*/
+
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/client/index.tsx"),
+  // Designate entrypoint
+  entry: path.resolve(__dirname, "src/background/index.ts"),
+
+  // Designate output
   output: {
     path: path.resolve(__dirname, "extension/chrome/build"),
-    filename: "bundle.js",
+    filename: "background.js",
     publicPath: "/",
   },
+
+  // Configure source maps for development environment
   devtool: "inline-source-map",
+
+  // Optimize build for development environment
   mode: "development",
+
+  // Configure rules for module use
   module: {
     rules: [
+      // For JavaScript
       {
-        // Testing for any .js/.jsx files to be transpiled by Babel preset-react, and to transpile down
-        // any ES6+ code down to version that can be compatible with any browser
         test: /\.(js|jsx)$/i,
         exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", `@babel/preset-react`],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
-      {
-        // Testing for any .css/.scss files so that webpack can fulfill the style import in 'index.js'
-        test: /\.(scss)$/i,
-        exclude: /(node_modules)/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+
+      // For TypeScript
       {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
         use: ["ts-loader"],
       },
+
+      // For CSS
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      // For Sass
+      {
+        test: /\.(scss)$/i,
+        exclude: /(node_modules)/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+
+      // For images, fonts, and other file types smaller than 8192 bytes
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg|ico)$/,
         use: [
           {
-            // loads files as base64 encoded data url if image file is less than set limit
             loader: "url-loader",
             options: {
-              // if file is greater than the limit (bytes), file-loader is used as fallback
               limit: 8192,
             },
           },
@@ -51,16 +72,19 @@ module.exports = {
       },
     ],
   },
+
+  // Configure plugins
   plugins: [
-    // Generates an HTML file based on the template we pass in to serve our webpack files
-    // which in this case, the template is our own 'index.html' file
+
+    // For generating an HTML file from a template
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "./src/client/index.html"),
+      filename: "background.html",
+      template: path.resolve(__dirname, "./src/background/index.html"),
     }),
   ],
+
+  // Scope output file extensions
   resolve: {
-    // Enable importing .js and .jsx files without specifying their extension
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 };
